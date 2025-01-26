fbd8a06d3d4ce5088d59fecec08e0043900f334f
BrandAlex117 <hryugaiden@gmail.com>
2025-01-25 21:50:37 -0600

    new progress

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
+import { useState } from 'react';
+import type { LinksProps } from 'src/data/links';
 
 interface HeaderProps {
   title: string;
+  Links: LinksProps[];
 }
 
-const Header = ({ title }: HeaderProps) => {
+const Header = ({ title, Links, }: HeaderProps) => {
+
+ 
+
   return (
     <header className='bg-black w-full p-4'>
-      <h1 className='text-4xl text-yellow-500 font-bold'>{title}</h1>
+      <nav className='max-w-screen-2xl mx-auto flex gap-6 items-center justify-between'>
+      <h1 className='text-4xl text-yellow-500 font-bold '>{title}</h1>
+
+      <ul className='flex gap-3 font-medium'>
+        {
+          Links.map((link) => (
+            <li key={link.url} className='text-yellow-500'>
+              {
+                link.label === "Comprar" ? (
+                  <div
+                  // onMouseEnter={handleDropdownToggle}  // Muestra el dropdown al pasar el mouse
+                  // onMouseLeave={handleDropdownToggle}  // Oculta el dropdown cuando el mouse sale
+                >
+                  <a href={link.url}>{link.label}</a>
+                  {(
+                    <ul className="dropdown">
+                      <li><a href="/servicios/desarrollo-web">Desarrollo Web</a></li>
+                      <li><a href="/servicios/seo">SEO</a></li>
+                      <li><a href="/servicios/diseno">Diseño Gráfico</a></li>
+                    </ul>
+                  )}
+                </div>
+                ) : (
+                  <a href={link.url}>{link.label}</a> 
+                )}
+            </li>
+          ))
+        }
+      </ul>
+
+      </nav>
     </header>
   )
 }
---
+++ src/data/links.ts
@@ -0,0 +1,13 @@
+export interface LinksProps {
+  label: string;
+  url: string;
+}
+
+export const Links: LinksProps[] = [
+  { label: 'Inicio', url: '/' },
+  { label: 'Comprar', url: '/tienda' },
+  { label: 'Aprender', url: '/Aprender' },
+  { label: 'Planes', url: '/Planes' },
+  { label: 'Blogs', url: '/Blogs' },
+  { label: 'Contacto', url: '/contacto' },
+];
